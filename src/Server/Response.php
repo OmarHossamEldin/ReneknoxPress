5d9ea6b6fb2d94efa47dce6c5709c24283a148a6
Omar Kandil <okandil273@gmail.com>
2023-04-29 04:51:31 +0200

    fix-response-header-statuscode

--- src/Server/Response.php
+++ src/Server/Response.php
@@ -10,9 +10,12 @@ class Response
 {
     private $action;
 
+    private Header $header;
+
     public function __construct($action)
     {
         $this->action = $action;
+        $this->header = new Header();
     }
 
     public function set_status_code(int $statusCode): void
